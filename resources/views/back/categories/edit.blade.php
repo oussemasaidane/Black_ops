feeaae3b6794a9685a6fbeb92930f7d37092f9eb
Aziz Ch <98626462+M0Aziz@users.noreply.github.com>
2023-09-30 20:10:11 +0200

    Update edit.blade.php

--- resources/views/back/categories/edit.blade.php
+++ resources/views/back/categories/edit.blade.php
@@ -3,14 +3,15 @@
 @section('content')
     <div class="container">
         <h2>Edit Categorie</h2>
-        <form action="{{ route('categories.update', ['categorie' => $categorie->id]) }}" method="POST">
+         <form action="{{ route('categories.update', ['categorie' => $categorie->id]) }}" method="POST">
             @csrf
             @method('PUT')
             <div class="form-group">
                 <label for="nom">Nom:</label>
-                <input type="text" class="form-control" id="nom" name="nom" value="{{ $categorie->nom }}" required>
+                <input type="text" style=" 
+                border: 1px solid gray; width:50%" class="form-control" id="nom" name="nom" value="{{ $categorie->nom }}" required>
             </div>
-            <button type="submit" class="btn btn-primary">Update</button>
+            <button type="submit" class="btn btn-primary mt-5">Update</button>
         </form>
     </div>
 @endsection
